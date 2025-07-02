76e7c524a3e64343ce3a0916ed158f2ae9e31391
SergProg08 <rybakvasilii1608@gmail.com>
2025-07-02 16:21:01 +0300

    update cities

--- src/api/v1/cities.py
+++ src/api/v1/cities.py
@@ -1,7 +1,13 @@
+from http.client import HTTPException
+import uuid
+
+from api.dependencies import UOWDep
 from fastapi import APIRouter, Depends
 from typing import List
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from schemas.cities import CitySchemaAdd, CitySchema, CitySchemaUpdate
+from service.cities import CitiesService
 
 from database import get_session
 from settings import settings
@@ -12,32 +18,50 @@ router = APIRouter(
 )
 
 @router.get(
-    "/",
-    response_model=List[CitiesGroup],
-    summary="Получить список городов"
+    "/{city_id}",
+    response_model=CitySchema,
+    summary="Получить город по id"
 )
-async def get_all_cities(
-    db: AsyncSession = Depends(get_session),
+async def get_city_by_id(
+    city_id: uuid.UUID,
+    uow: UOWDep,
 ):
-
+    city = await CitiesService().get_city_by_id(city_id, uow)
+    if not city:
+        raise HTTPException(
+            status_code=404,
+            detail={"success": False, "message": "City not found"}
+        )
+    return city
 
 
 @router.post(
-    "/new_city",
-    response_model=List[CitiesGroup],
-    summary="Добавить новый город"
+    "/",
+    response_model=dict,
+    summary="Добавить новый город",
+    description="Создает запись о городе в базе данных"
 )
-async def add_new_city(
-    db: AsyncSession = Depends(get_session),
-):
+async def add_city(
+    city: CitySchemaAdd,
+    uow: UOWDep,
+)-> dict:
+    city_id = await CitiesService().add_city(uow, city)
+    return{
+        "success": True,
+        "message": "Город успешно добавлен",
+        "data": {"city_id": city_id}
+    }
     
 
-
 @router.patch(
-    "/change_city",
-    response_model=List[CitiesGroup],
-    summary="Изменить список городов"
+    "/{city_id}",
+    response_model=CitySchema,
+    summary="Изменить город"
 )
-async def change_cities(
-    db: AsyncSession = Depends(get_session),
+async def patch_city(
+    city_id: uuid.UUID,
+    update_data: CitySchemaUpdate,
+    uow: UOWDep
 ):
+    city = await CitiesService().update_city(uow, city_id, update_data)
+    return city
